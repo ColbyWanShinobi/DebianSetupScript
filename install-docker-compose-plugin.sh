bcd25d8bff855b6012a875157679d865643f7dc2
Colby Butler <colby@owljet.com>
2026-04-08 17:35:22 -0400

    Fixed docker plugin script

--- install-docker-compose-plugin.sh
+++ install-docker-compose-plugin.sh
@@ -4,6 +4,7 @@ set -e
 #set -x
 
 CODENAME=$(grep CODENAME /etc/os-release | cut -d'=' -f2)
+DISTRO=$(grep ID /etc/os-release | grep -v VERSION | cut -d'=' -f2)
 
 sudo rm -rfv /etc/apt/keyrings/docker.gpg /etc/apt/sources.list.d/docker.list | true
 
@@ -13,13 +14,13 @@ sudo apt install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 
 if [ ! -f "/etc/apt/keyrings/docker.gpg" ]; then
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ curl -fsSL https://download.docker.com/linux/${DISTRO}/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
  sudo chmod a+r /etc/apt/keyrings/docker.gpg
 fi
 
 if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
  echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/${DISTRO} \
   "$(cat /etc/os-release | grep CODENAME | head -n 1 | cut -f2 -d=)" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
